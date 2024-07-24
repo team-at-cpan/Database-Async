d33d8a216166922eb1dc91da8c5c919ab0c10843
Tom Molesworth <TEAM@cpan.org>
2024-07-25 01:46:11 +0800

    Convert to proper Object::Pad class

--- lib/Database/Async/Pool.pm
+++ lib/Database/Async/Pool.pm
@@ -5,7 +5,7 @@ use warnings;
 
 use Object::Pad;
 class Database::Async::Pool;
-inherit IO::Async::Notifier;
+inherit IO::AsyncX::Notifier;
 
 # VERSION
 
@@ -27,9 +27,20 @@ use Scalar::Util qw(blessed refaddr);
 use List::UtilsBy qw(extract_by);
 use Log::Any qw($log);
 
-sub new {
-    my ($class, %args) = @_;
-    my $backoff = delete $args{backoff};
+field $backoff:reader;
+field $pending_count:reader = 0;
+field $count:reader         = 0;
+field $min:reader           = 0;
+field $max:reader           = 1;
+field $attempts      = undef;
+field $ordering      = 'serial';
+field $waiting       { [] }
+field $ready         { [] }
+field $new_future;
+field $request_engine;
+field $uri;
+
+ADJUST {
     unless(blessed $backoff) {
         my $type = 'exponential';
         $type = $backoff if $backoff and not ref $backoff;
@@ -38,49 +49,33 @@ sub new {
             initial_delay => 0.010,
             max_delay     => 30,
             ($backoff && ref($backoff) ? %$backoff : ())
-        )
+        );
     }
-    bless {
-        pending_count => 0,
-        count         => 0,
-        min           => 0,
-        max           => 1,
-        attempts      => undef,
-        ordering      => 'serial',
-        backoff       => $backoff,
-        waiting       => [],
-        ready         => [],
-        %args
-    }, $class
 }
 
-sub min { shift->{min} }
-sub max { shift->{max} }
-sub count { shift->{count} }
-sub pending_count { shift->{pending_count} }
-sub backoff { shift->{backoff} }
-
-sub register_engine {
-    my ($self, $engine) = @_;
-    --$self->{pending_count};
-    ++$self->{count};
+method register_engine ($engine) {
+    --$pending_count;
+    ++$count;
     $self
 }
 
-sub unregister_engine {
-    my ($self, $engine) = @_;
+method unregister_engine ($engine) {
     try {
-        $log->tracef('Engine is removed from the pool, with %d in the queue', 0 + @{$self->{waiting}});
+        $log->tracef('Engine is removed from the pool, with %d in the queue', 0 + $waiting->@*);
         my $addr = refaddr($engine);
         # This engine may have been actively processing a request, and not in the pool:
         # that's fine, we only remove if we had it.
-        my $count = () = extract_by { refaddr($_) == $addr } @{$self->{ready}};
-        $log->tracef('Removed %d engine instances from the ready pool', $count);
+        my $matching_count = () = extract_by { refaddr($_) == $addr } $ready->@*;
+        $log->tracef('Removed %d engine instances from the ready pool', $matching_count);
         # Any engine that wasn't in the ready queue (`count`) was out on assignment
         # and thus included in `pending_count`
-        --$self->{$count ? 'count' : 'pending_count'};
-        $log->infof('After cleanup we have %d count, %d pending, %d waiting', $self->{count}, $self->{pending_count}, 0 + @{$self->{waiting}});
-        $self->adopt_future($self->process_pending) if @{$self->{waiting}};
+        if($matching_count) {
+            --$count;
+        } else {
+            --$pending_count;
+        }
+        $log->infof('After cleanup we have %d count, %d pending, %d waiting', $count, $pending_count, 0 + $waiting->@*);
+        $self->adopt_future($self->process_pending) if $waiting->@*;
     } catch ($e) {
         $log->errorf('Failed %s', $e);
     }
@@ -93,11 +88,10 @@ Called when there's a spare engine we can put back in the pool.
 
 =cut
 
-sub queue_ready_engine {
-    my ($self, $engine) = @_;
-    $log->tracef('Engine is now ready, with %d in the queue', 0 + @{$self->{waiting}});
-    return $self->notify_engine($engine) if @{$self->{waiting}};
-    push @{$self->{ready}}, $engine;
+method queue_ready_engine ($engine) {
+    $log->tracef('Engine is now ready, with %d in the queue', 0 + $waiting->@*);
+    return $self->notify_engine($engine) if $waiting->@*;
+    push $ready->@*, $engine;
     $self
 }
 
@@ -108,10 +102,9 @@ waiting request.
 
 =cut
 
-sub notify_engine {
-    my ($self, $engine) = @_;
+method notify_engine ($engine) {
     die 'unable to notify, we have no pending requests'
-        unless my $f = shift @{$self->{waiting}};
+        unless my $f = shift $waiting->@*;
     $f->done($engine);
     return $self;
 }
@@ -122,38 +115,34 @@ Resolves to an engine. May need to wait if there are none available.
 
 =cut
 
-async sub next_engine {
-    my ($self) = @_;
-    $log->tracef('Have %d ready engines to use', 0 + @{$self->{ready}});
-    if(my $engine = shift @{$self->{ready}}) {
+async method next_engine {
+    $log->tracef('Have %d ready engines to use', 0 + $ready->@*);
+    if(my $engine = shift $ready->@*) {
         return $engine;
     }
-    push @{$self->{waiting}}, my $f = $self->new_future;
+    push $waiting->@*, my $f = $self->new_future;
     await $self->process_pending;
     return await $f;
 }
 
-async sub process_pending {
-    my ($self) = @_;
-    my $total = $self->count + $self->pending_count;
-    $log->tracef('Might request, current count is %d/%d (%d pending, %d active)', $total, $self->max, $self->pending_count, $self->count);
+async method process_pending {
+    my $total = $count + $pending_count;
+    $log->tracef('Might request, current count is %d/%d (%d pending, %d active)', $total, $self->max, $pending_count, $count);
     await $self->request_engine unless $total >= $self->max;
     return;
 }
 
-sub new_future {
-    my ($self, $label) = @_;
+method new_future ($label = 'pool') {
     (
-        $self->{new_future} //= sub {
+        $new_future //= sub {
             Future->new->set_label($_[1])
         }
     )->($label)
 }
 
-async sub request_engine {
-    my ($self) = @_;
+async method request_engine {
     $log->tracef('Pool requesting new engine');
-    ++$self->{pending_count};
+    ++$pending_count;
     my $delay = $self->backoff->next;
     if($delay) {
         my $f = $self->loop->delay_future(
@@ -162,16 +151,15 @@ async sub request_engine {
         CANCEL { $f->cancel }
         await $f;
     }
-    my $req = $self->{request_engine}->();
+    my $req = $request_engine->();
     CANCEL { $req->cancel }
     await $req;
     $self->backoff->reset;
 }
 
-sub _remove_from_loop {
-    my ($self, $loop) = @_;
-    $_->cancel for splice @{$self->{waiting}};
-    $self->unregister_engine($_) for splice @{$self->{ready}};
+method _remove_from_loop ($loop) {
+    $_->cancel for splice $waiting->@*;
+    $self->unregister_engine($_) for splice $ready->@*;
     return $self->next::method($loop);
 }
 
